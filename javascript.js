334ac45b47fc1a3dc4a1347011c0fd6b544e2ef1
ajarred <69410244+ajarred@users.noreply.github.com>
2022-12-27 15:06:47 +0800

    Add game logic

--- javascript.js
+++ javascript.js
@@ -18,12 +18,58 @@ function getPlayerChoice() {
     let input = prompt();
     let filteredInput = input.toUpperCase();
     if (filteredInput === "ROCK" || filteredInput === "PAPER" || filteredInput === "SCISSORS") {
-        return filteredInput
+        return filteredInput;
     }
     else 
     window.alert("Invalid Input. Try Again.");
     getPlayerChoice();
 }
 
-console.log(getPlayerChoice());
+function playRound(playerSelection,computerSelection) {
+    let currentMatch = `${playerSelection} vs ${computerSelection}`;
+    let win = `${currentMatch} = YOU WIN`;
+    let lose = `${currentMatch} = YOU LOSE`;
+    // DRAW
+    if (playerSelection === computerSelection) {
+        alert("DRAW");
+        return;
+    }
+    else {
+    // ROCK
+    if (playerSelection === "ROCK") {
+        if (computerSelection === "SCISSORS") {
+            alert(win);
+        }
+        else {
+            alert(lose);
+        }
+    }
+    // SCISSORS
+    if (playerSelection === "SCISSORS") {
+        if (computerSelection === "PAPER") {
+            alert(win);
+        }
+        else {
+            alert(lose);
+        }
+    }
+    // PAPER 
+    if (playerSelection === "PAPER") {
+        if (computerSelection === "ROCK") {
+            alert(win);
+        }
+        else {
+            alert(lose);
+        }
+    }
+}
+}
+
+let playerSelection = getPlayerChoice();
+let computerSelection = getComputerChoice();
+
+console.log(playerSelection);
+console.log(computerSelection);
+console.log(playRound(playerSelection,computerSelection));
+
 

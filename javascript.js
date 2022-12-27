6c8df56114ca23fe0867d017efd08e40c88fcaf0
ajarred <69410244+ajarred@users.noreply.github.com>
2022-12-27 13:57:53 +0800

    Add computer choices

--- javascript.js
+++ javascript.js
@@ -1,6 +1,18 @@
 
 function getComputerChoice(){
     let computerChoice = Math.floor(Math.random()*3);
+    switch(computerChoice) {
+        case 0:
+            computerChoice = "ROCK";
+            break;
+        case 1:
+            computerChoice = "PAPER";
+            break;
+        case 2:
+            computerChoice = "SCISSORS";
+            break;
+    }
+    return computerChoice;
 }
 
 console.log(getComputerChoice());

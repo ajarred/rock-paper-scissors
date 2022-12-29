389366d4cda26fa0d350f82bb32c2b1ebc747288
ajarred <69410244+ajarred@users.noreply.github.com>
2022-12-29 20:35:04 +0800

    Complete game logic

--- javascript.js
+++ javascript.js
@@ -1,64 +1,43 @@
 let W = 0;
 let L = 0;
 
+const buttons = document.querySelectorAll('input');
+
 function getComputerChoice(){
-    let computerChoice = Math.floor(Math.random()*3);
-    switch(computerChoice) {
-        case 0:
-            computerChoice = "ROCK";
-            break;
-        case 1:
-            computerChoice = "PAPER";
-            break;
-        case 2:
-            computerChoice = "SCISSORS";
-            break;
-    }
-    return computerChoice;
+    let choices = ['ROCK', 'PAPER', 'SCISSORS'];
+    return choices[Math.floor(Math.random() * choices.length)];
 }
 
-/* function getPlayerChoice() {
-    let input = prompt();
-    let filteredInput = input.toUpperCase();
-    if (filteredInput === "ROCK" || 
-        filteredInput === "PAPER" || 
-        filteredInput === "SCISSORS") {
-        return filteredInput;
-    }
-    else 
-    window.alert("Invalid Input. Try Again.");
-    getPlayerChoice();
-} */
+function disableButtons() {
+    buttons.forEach(elem => {
+        elem.disabled = true;
+    })
+}
 
-function playRound(playerSelectionn) {
-    let computerSelection = computerChoice();
-    let currentMatch = `(YOU)${playerSelection} vs ${computerSelection}`;
-    let draw = `${currentMatch} = DRAW`;
-    let win = `${currentMatch} = YOU WIN`;
-    let lose = `${currentMatch} = YOU LOSE`;
-    let record = `Current Record: ${W}-${L}`;
-    let log = '';
-    
+function playRound(playerSelection) {
+    let computerSelection = getComputerChoice();
 
-    if (playerSelection === computerSelection) {
-        log = draw.concat("\n",record);
-    }
-    else if 
-        ((playerSelection === "ROCK" && computerSelection === "SCISSORS") || 
-        (playerSelection === "SCISSORS" && computerSelection === "PAPER") ||
-        (playerSelection === "PAPER" && computerSelection === "ROCK")) {
-        W++;
-        log = win.concat("\n",record);
+    if ((playerSelection == "ROCK" && computerSelection =="SCISSORS") || 
+        (playerSelection =="SCISSORS" && computerSelection == "PAPER") ||
+        (playerSelection =="PAPER" && computerSelection == "ROCK")) {
+        W+=1;
+        log =  `(YOU)${playerSelection} vs ${computerSelection}= WIN, 
+        Current Record: ${W}-${L}`;
         if (W==5) {
-        log = "YOU WIN";
-        disableButtons();
+            log += "\nYOU WIN!";
+            disableButtons();
         }
+        }
+    else if (playerSelection == computerSelection) {
+        log = log = `(YOU)${playerSelection} vs ${computerSelection}= DRAW, 
+        Current Record: ${W}-${L}`;
         }
     else {
-        L++;
-        log = lose.concat("\n",record);
+        L+=1;
+        log = `(YOU)${playerSelection} vs ${computerSelection}= LOSE, 
+            Current Record: ${W}-${L}`;
         if (L==5) {
-        log = "YOU LOSE";
+            log += "\nYOU LOSE!";
         disableButtons();
         }
     }
@@ -66,36 +45,8 @@ function playRound(playerSelectionn) {
     return;
 }
 
-/*function game() {
-    // for (let i=0; i<5; i++) {
-       // let playerSelection = getPlayerChoice();
-        let computerSelection = getComputerChoice();
-        playRound(playerSelection,computerSelection);
-    }
-// } */ 
-
-function disableButtons() {
-    buttons.forEach(elem => {
-        elem.disabled = true
-    })
-}
-
-const buttons = document.querySelectorAll('.btn');
-
-const rock = document.querySelector('#rock');
-rock.addEventListener('click', () => {
-    playerSelection = "ROCK";
-    game();
-});
-
-const paper = document.querySelector('#paper');
-paper.addEventListener('click', () => {
-    playerSelection = "PAPER";
-    game();
-});
-
-const scissors = document.querySelector('#scissors');
-scissors.addEventListener('click', () => {
-    playerSelection = "SCISSORS";
-    game();
+buttons.forEach(button => {
+    button.addEventListener('click', function(){
+        playRound(button.value);
+    });
 });

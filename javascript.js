b573994e16b1f09503d75431af9cde7b02f9f0bb
ajarred <69410244+ajarred@users.noreply.github.com>
2022-12-30 11:42:45 +0800

    Add output

--- javascript.js
+++ javascript.js
@@ -4,6 +4,9 @@ let log = '';
 let record = '';
 
 const buttons = document.querySelectorAll('.button');
+const output = document.querySelector('#output');
+
+output.textContent = "GLHF<3";
 
 function getComputerChoice(){
     let choices = ['ROCK', 'PAPER', 'SCISSORS'];
@@ -16,26 +19,31 @@ function playRound(playerSelection) {
     if ((playerSelection == "ROCK" && computerSelection =="SCISSORS") || 
         (playerSelection =="SCISSORS" && computerSelection == "PAPER") ||
         (playerSelection =="PAPER" && computerSelection == "ROCK")) {
-        W+=1;
-        log = `(YOU)${playerSelection} vs ${computerSelection}= WIN`;
+        W++;
+        log = `(YOU)${playerSelection} vs ${computerSelection}(COMPUTER)= WIN`;
         record = `Current Record: ${W}-${L}`;
+        output.textContent = "NICE";
         if (W==5) {
-            log += "\nYOU WIN!";
+            log += "</br>YOU WIN!";
+            output.textContent = "GGS! CONGRATS";
             W = 0;
             L = 0;
             record = `Current Record: ${W}-${L}`;
         }
         }
     else if (playerSelection == computerSelection) {
-        log = `(YOU)${playerSelection} vs ${computerSelection}= DRAW`;
+        log = `(YOU)${playerSelection} vs ${computerSelection}(COMPUTER)= DRAW`;
         record = `Current Record: ${W}-${L}`;
+        output.textContent = "WOW";
         }
     else {
-        L+=1;
-        log = `(YOU)${playerSelection} vs ${computerSelection}= LOSE`;
+        L++;
+        log = `(YOU)${playerSelection} vs ${computerSelection}(COMPUTER)= LOSE`;
         record = `Current Record: ${W}-${L}`;
+        output.textContent = "OOF";
         if (L==5) {
-            log += "\nYOU LOSE!";
+            log += "<br>YOU LOSE!";
+            output.textContent = "GGS! BETTER LUCK NEXT TIME";
             W = 0;
             L = 0;
             record = `Current Record: ${W}-${L}`;
